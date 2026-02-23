4faf7ca2ce626b725cd45fc6002152bf97d6fe13
Wong Mei Kuan <meikuanwong4@gmail.com>
2026-02-23 13:49:25 +0800

    Add in github and add names below icon

--- src/components/Skillset/Techstack.jsx
+++ src/components/Skillset/Techstack.jsx
@@ -5,7 +5,6 @@ import {
   DiReact,
   DiJava,
   DiCss3,
-  DiHtml5
 } from "react-icons/di";
 import {
   SiHtml5,
@@ -21,36 +20,43 @@ const Techstack = () => {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiHtml5 />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>HTML5</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>JavaScript</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiCplusplus />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>C++</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>React</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Java</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiCss3 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiHtml5 />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>CSS3</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Firebase</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiDart />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Dart</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFlutter />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Flutter</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFastapi />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>FastAPI</p>
       </Col>
     </Row>
   );
--- src/components/Skillset/Toolstack.jsx
+++ src/components/Skillset/Toolstack.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { React } from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
   SiVercel,
-  SiAndroidstudio 
+  SiAndroidstudio,
+  SiGooglecolab,
+  SiGithub 
 } from "react-icons/si";
 import { DiVisualstudio } from "react-icons/di";
 
@@ -14,18 +16,31 @@ const Toolstack = () => {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>VS Code</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Postman</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Vercel</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiVisualstudio  />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Visual Studio 2022</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAndroidstudio  />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Android Studio</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglecolab  />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>Google Colab</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub  />
+        <p style={{ fontSize: "16px", fontWeight: "bold", marginTop: "10px" }}>GitHub</p>
       </Col>
     </Row>
   );
